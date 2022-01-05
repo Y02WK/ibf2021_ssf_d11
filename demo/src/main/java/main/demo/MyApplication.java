7587d0de8d575813aca8b62d4355a10d6747ca79
Y02WK <59220086+Y02WK@users.noreply.github.com>
2022-01-05 16:30:59 +0800

    Added comments.

--- demo/src/main/java/main/demo/MyApplication.java
+++ demo/src/main/java/main/demo/MyApplication.java
@@ -2,6 +2,8 @@ package main.demo;
 
 import java.util.Collections;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.DefaultApplicationArguments;
 import org.springframework.boot.SpringApplication;
@@ -10,17 +12,31 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MyApplication {
 
+	private static final Logger logger = LoggerFactory.getLogger(MyApplication.class);
+	private static final String DEFAULT_PORT = "3000";
+
 	public static void main(String[] args) {
+		// init Spring app
 		SpringApplication app = new SpringApplication(MyApplication.class);
 
+		// decode app args using Spring args helper
 		ApplicationArguments cliArgs = new DefaultApplicationArguments(args);
 
-		String port = "3000";
+		// init port as default port value
+		String port = DEFAULT_PORT;
 
+		// checks if cli contains port option
 		if (cliArgs.containsOption("port")) {
 			port = cliArgs.getOptionValues("port").get(0);
+			// else checks if PORT environment variable is set
+		} else if (System.getenv("PORT") != null) {
+			port = System.getenv("PORT");
 		}
 
+		// log port
+		logger.info("Port is " + port);
+
+		// override spring boot app server port to the port specified
 		app.setDefaultProperties(Collections.singletonMap("server.port", port));
 		app.run(args);
 	}
